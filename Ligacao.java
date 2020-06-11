231e706f01ef236429c1a0075e723c08d245448b
brunomurca180100399 <65693031+brunomurca180100399@users.noreply.github.com>
2020-06-11 23:53:57 +0100

    Update Ligacao.java

--- Ligacao.java
+++ Ligacao.java
@@ -3,26 +3,12 @@ package TrabalhoLP;
 public class Ligacao implements Comparable<Ligacao> {
 
 	protected double horas;
-	private Cidade origem;
 	private Cidade destino;
 
 	// Construtores
-	public Ligacao(Cidade origem, Cidade destino, double horas) {
-		this.origem = origem;
+	public Ligacao(Cidade destino, double horas) {
 		this.destino = destino;
 		this.horas = horas;
-	}
-
-	public Ligacao(Cidade origem, Cidade destino) {
-		this.origem = origem;
-		this.destino = destino;
-		horas = 0;
-	}
-
-	public Ligacao() {
-		origem = null;
-		destino = null;
-		horas = 0;
 	}
 
 	// Getters e setters
@@ -42,17 +28,9 @@ public class Ligacao implements Comparable<Ligacao> {
 		return destino;
 	}
 
-	public void setOrigem(Cidade origem) {
-		this.origem = origem;
-	}
-
-	public Cidade getOrigem() {
-		return origem;
-	}
-
 	@Override
 	public String toString() {
-		return String.format("(%s -> %s, %f)", origem.getNome(), destino.getNome(), horas);
+		return String.format("(%s, %f)", destino.getNome(), horas);
 	}
 
 	public int compareTo(Ligacao outraLigacao) {
