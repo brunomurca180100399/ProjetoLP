69f5aa7ddfc5039823123d617641beb14f54f262
brunomurca180100399 <65693031+brunomurca180100399@users.noreply.github.com>
2020-06-12 00:03:38 +0100

    Update IGrafo.java

--- IGrafo.java
+++ IGrafo.java
@@ -1,12 +1,13 @@
 package TrabalhoLP;
 
+import java.util.*;
+
 public interface IGrafo {
 
 	public int addCidade(String nome);
 
-	public void addLigacao(Cidade origem, Cidade destino, double horas);
+	public void encontraMenorCaminho(Cidade origem);
 
-	public void imprimeLigacoes();
+	public List<Cidade> criarCaminho(Cidade destino);
 
-	public void encontraMenorCaminho(Cidade origem, Cidade destino);
 }
